7d541cf7bf59a53183dd9b6671ee718fd36d6879
Maria Fernanda Castilho <castilhomariafer13@gmail.com>
2024-06-10 09:26:12 -0300

    Renomeado h3 páginas, index, cadastro e login.

--- login.php
+++ login.php
@@ -39,7 +39,7 @@
   <div class="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
     <header class="mb-auto">
       <div>
-        <h3 class="float-md-start mb-0">Cover</h3>
+        <h3 class="float-md-start mb-0">Login</h3>
         <nav class="nav nav-masthead justify-content-center float-md-end">
           <a class="nav-link" href="index.php">Página Inicial</a>
           <a class="nav-link" href="cadastro.php">Cadastre-se</a> 
